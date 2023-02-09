60eb3b972efdcb8f9d651239f4c1873fb0053c07
Rodrigo Quaresma <rodrigo.quaresma.souza@gmail.com>
2023-02-09 18:52:41 -0300

    Criacao das entidades

---
+++ ConsoleApp1/Entities/Contract.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1.Entities
+{
+    internal class Contract
+    {
+        public int Number { get; set; }
+        public DateTime Date { get; set; }
+        public double TotalValue { get; set; }
+        public List<Installment> Installments { get; set; }
+
+        public Contract(int number, DateTime date, double totalValue)
+        {
+            Number = number;
+            Date = date;
+            TotalValue = totalValue;
+            Installments = new List<Installment>();
+        }
+
+        public void AddInstalments(Installment installment)
+        {
+            Installments.Add(installment);
+        }
+
+
+    }
+}
---
+++ ConsoleApp1/Entities/Installment.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp1.Entities
+{
+    internal class Installment
+    {
+        public DateTime DueDate { get; set; }
+        public double Amount { get; set; }
+
+        public Installment(DateTime dueDate, double amount)
+        {
+            DueDate = dueDate;
+            Amount = amount;
+        }
+    }
+}
